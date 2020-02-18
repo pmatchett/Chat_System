583467d07f6adfb0edc06fbe2ee3b676ba5e3542
pmatchett <pmatchett@shaw.ca>
2020-02-17 17:42:20 -0700

    can send message with enter key

--- Client/chatScript.js
+++ Client/chatScript.js
@@ -7,4 +7,12 @@ $(document).ready(function(){
   socket.on("chat message", function(msg){
     $("#messages").append($("<li>").text(msg));
   });
+  //can send message with enter key
+  $("#m").keypress(function(event){
+    let keyCode = event.key;
+    if(keyCode == 'Enter'){
+      socket.emit("chat message", $('#m').val());
+      $("#m").val("");
+    }
+  });
 });
