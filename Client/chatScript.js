dae268f94cc382edc21be8b509d9f94ecac227b8
pmatchett <pmatchett@shaw.ca>
2020-03-08 16:27:45 -0600

    Chat system ready for submission

--- Client/chatScript.js
+++ Client/chatScript.js
@@ -1,18 +1,159 @@
+"use strict"
 let socket = io();
 $(document).ready(function(){
+
+  //users current nickname
+  let username;
+  //user's current color for their nickname (will start as black)
+  let nickcolor = "000000";
+
+  //TODO: check if message is empty before sending
+  //sending message with the send button is clicked
   $("#send").click(function(){
-    socket.emit("chat message", $('#m').val());
-    $("#m").val("");
+    handleInput();
   });
-  socket.on("chat message", function(msg){
-    $("#messages").append($("<li>").text(msg));
-  });
+
   //can send message with enter key
-  $("#m").keypress(function(event){
+  $("#input").keypress(function(event){
     let keyCode = event.key;
     if(keyCode == 'Enter'){
-      socket.emit("chat message", $('#m').val());
-      $("#m").val("");
+      handleInput();
     }
   });
+
+  function isHexColor(hex){
+    if(hex.length===6){
+      if(!isNaN(Number('0x' + hex))){
+        return true;
+      }
+    }
+    return false;
+  }
+
+  function handleInput(){
+    let message = {
+      text: $('#input').val(),
+      nickname: username,
+      usercolor: nickcolor
+    }
+    //don't send the message if the content is empty
+    if(message.text === ""){
+      return;
+    }
+    //checking if user is sending a command
+    if(message.text[0] === "/"){
+      //change nickname message
+      if(message.text.startsWith("/nick ")){
+        message.text = message.text.slice(6, message.text.length);
+        let nameData = {
+          newName: message.text,
+          oldName: username
+        };
+        socket.emit("change name", nameData);
+      }
+      else if(message.text.startsWith("/nickcolor ")){
+        let color = message.text.slice(11, message.text.length);
+        if(isHexColor(color)){
+          nickcolor = color;
+          $("#messages").prepend($("<li class='warning'>").html("<b><i>**Succesfully changed color to: <span style='color:#"+color+"'>"+username+"</span></i></b>"));
+          let height = $("#messages")[0].scrollHeight;
+          $("#messages").scrollTop(height);
+        }
+        else{
+          $("#messages").prepend($("<li class='warning'>").html("<b><i>**Must input a hex color</i></b>"));
+          let height = $("#messages")[0].scrollHeight;
+          $("#messages").scrollTop(height);
+        }
+      }
+      else{
+          $("#messages").prepend($("<li class='warning'>").html("<b><i>**invalid command, accepted commands: /nick new name, /nickcolor hex color</i></b>"));
+          let height = $("#messages")[0].scrollHeight;
+          $("#messages").scrollTop(height);
+      }
+    }
+    else{
+      socket.emit("chat message", message);
+    }
+    $("#input").val("");
+  }
+
+  //receiving a message from the server
+  socket.on("chat message", function(msg){
+    let currentDate = new Date(msg.time);
+    let minutes = currentDate.getMinutes();
+    if(minutes < 10){
+      minutes = "0"+minutes;
+    }
+    let currentTime = currentDate.getHours() + ":" + minutes;
+    $("#messages").prepend($("<li class='otherMess'>").html("<span class='info'>"+currentTime + " <span style='color:#"+msg.usercolor+"'>" + msg.user + "</span>: </span><br><span class='other'>" + msg.text+"</span>"));
+    let height = $("#messages")[0].scrollHeight;
+    $("#messages").scrollTop(height);
+  });
+
+  socket.on("own message", function(msg){
+    let currentDate = new Date(msg.time);
+    let minutes = currentDate.getMinutes();
+    if(minutes < 10){
+      minutes = "0"+minutes;
+    }
+    let currentTime = currentDate.getHours() + ":" + minutes;
+    $("#messages").prepend($("<li class='ownMess'>").html("<b><span class='info'>:" + "<span style='color:#"+msg.usercolor+"'>" + msg.user + " </span>"+currentTime+"</span><br> <span class='own'>" + msg.text+"</span>" + "</b>"));
+    let height = $("#messages")[0].scrollHeight;
+    $("#messages").scrollTop(height);
+  });
+
+  socket.on("initial", function(msg){
+    console.log("connected");
+    for(let message of msg.chat){
+      let currentDate = new Date(message.time);
+      let minutes = currentDate.getMinutes();
+      if(minutes < 10){
+        minutes = "0"+minutes;
+      }
+      let currentTime = currentDate.getHours() + ":" + minutes;
+      $("#messages").prepend($("<li class='otherMess'>").html("<span class='info'>"+currentTime + " <span style='color:#"+message.usercolor+"'>" + message.user + "</span>:</span><br> <span class='other'>" + message.text+"</span>"));
+    }
+    let height = $("#messages")[0].scrollHeight;
+    $("#messages").scrollTop(height);
+    for(let user of msg.users){
+      $("#users").prepend($("<li>").html("<b>"+user.nick+"</b>"));
+    }
+    let userheight = $("#users")[0].scrollHeight;
+    $("#users").scrollTop(userheight);
+  });
+
+  socket.on("name", function(givenName){
+    username = givenName;
+    $("#nickname").text("You are: " + username);
+    document.cookie = "user="+username;
+  });
+
+  socket.on("user add", function(name){
+    $("#users").prepend($("<li>").html("<b>"+name+"</b>"));
+    let userheight = $("#users")[0].scrollHeight;
+    $("#users").scrollTop(userheight);
+  });
+
+  socket.on("user del", function(name){
+    $("#users>li").remove(":contains('"+name+"')");
+    let userheight = $("#users")[0].scrollHeight;
+    $("#users").scrollTop(userheight);
+  });
+
+  socket.on("name change fail", function(name){
+    $("#messages").prepend($("<li class='warning'>").html("<b><i>**" + name + " is already taken, please chose another name</i></b>"));
+    let height = $("#messages")[0].scrollHeight;
+    $("#messages").scrollTop(height);
+  });
+
+  socket.on("name change success", function(name){
+    $("#messages").prepend($("<li class='warning'>").html("<b><i>**" + "Changed name to: " + name+"</i></b>"));
+    let height = $("#messages")[0].scrollHeight;
+    $("#messages").scrollTop(height);
+    username = name;
+    $("#nickname").text("You are: " + username);
+    document.cookie = "user="+username;
+  });
+
+
 });
--- index.js
+++ index.js
@@ -1,8 +1,13 @@
+"use strict";
 let express = require('express');
 let app = express();
 let http = require('http').createServer(app);
 let io = require('socket.io')(http);
 let path = require('path');
+let cookie = require("cookie");
+
+let msgLog = initLog();
+let users = initNicks();
 
 
 app.use("/Client", express.static("Client"));
@@ -10,23 +15,141 @@ app.use("/Client", express.static("Client"));
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname + '/Client/index.html'));
 });
-app.get('/chatStyle.css', function(req, res) {
-  res.sendFile(path.join(__dirname + '/Client/chatStyle.css'));
-});
-app.get('/chatScript.js', function(req, res) {
-  res.sendFile(path.join(__dirname + '/Client/chatScript.js'));
-});
+
 
 io.on("connection", function(socket){
-  console.log("user has connected");
-  io.on("disconnect", function(socket){
-    console.log("User has disconnected");
+  let userId = socket.id;
+  console.log("user has connected "+ userId);
+
+  //sending the message log to a new connection
+  let info = {
+    chat:msgLog.getLog(),
+    users:users.getNames()
+  }
+  socket.emit("initial", info);
+  //checking if there is a cookie from a previous connection
+  let cookies
+  if(socket.handshake.headers.cookie !== undefined){
+    cookies = cookie.parse(socket.handshake.headers.cookie);
+  }
+  if(cookies!==undefined && cookies.user !== undefined){
+    if(users.checkDuplicate(cookies.user)){
+      socket.emit("name", users.generateName(userId));
+    }
+    else{
+      socket.emit("name", cookies.user);
+      users.addNick(cookies.user, userId);
+    }
+  }
+  else{
+    socket.emit("name", users.generateName(userId));
+  }
+  socket.on("disconnect", function(socket){
+    console.log("User has disconnected "+userId);
+    users.removeNameId(userId);
   });
   socket.on("chat message", function(msg){
-    io.emit("chat message", msg);
-    console.log(msg);
-  })
+    let currentTime = Date.now();
+    let username = msg.nickname;
+    let content = msg.text;
+    let color = msg.usercolor;
+    let message = {
+      time: currentTime,
+      user: username,
+      text: content,
+      usercolor: color
+    };
+    msgLog.addMsg(message);
+    socket.broadcast.emit("chat message", message);
+    socket.emit("own message", message);
+  });
+  socket.on("change name", function(name){
+    if(users.changeName(name.newName, name.oldName, userId) === false){
+      socket.emit("name change fail", name.newName);
+    }
+    else{
+      socket.emit("name change success", name.newName);
+    }
+  });
 });
+
+//setting up the chat log
+//Might try to find a vector to optimize (currently no limit to chat log length, might want to chance that)
+//currently this method requires the chat log to be global, might have to look into this
+function initLog(){
+  let chatLog = [];
+  let index = 0;
+  function addMsg(message){
+    chatLog[index] = message;
+    index++;
+  }
+  function getLog(){
+    return chatLog;
+  }
+  return {addMsg, getLog};
+}
+
+//initialising the list of users, currently all users are kept on the list
+//Similar to log this method leaves a global variable
+function initNicks(){
+  let nicknames = [];
+  let index = 0;
+  let users = 0;
+  function addNick(name, socket){
+    nicknames[index] = {
+      nick: name,
+      id: socket
+    };
+    index++;
+    io.emit("user add", name);
+  }
+  function generateName(socket){
+    let newName = "User" + users;
+    addNick(newName, socket);
+    users++;
+    return newName;
+  }
+  function removeName(name){
+    for(let tempIndex in nicknames){
+      if(nicknames[tempIndex].nick === name){
+        nicknames.splice(tempIndex, 1);
+        index--;
+      }
+    }
+    io.emit("user del", name);
+  }
+  function removeNameId(id){
+    let name;
+    for(let tempIndex in nicknames){
+      if(nicknames[tempIndex].id === id){
+        name = nicknames[tempIndex].nick;
+        nicknames.splice(tempIndex, 1);
+        index--;
+      }
+    }
+    io.emit("user del", name);
+  }
+  function changeName(newName, oldName, socket){
+    if(checkDuplicate(newName)){
+      return false;
+    }
+    addNick(newName, socket);
+    removeName(oldName);
+    return true;
+  }
+  function getNames(){
+    return nicknames
+  }
+  function checkDuplicate(name){
+    for(let tempIndex in nicknames){
+      if(nicknames[tempIndex].nick.toLowerCase() === name.toLowerCase()){
+        return true;
+      }
+    }
+    return false;
+  }
+  return {addNick, generateName, getNames, removeName, removeNameId, changeName, checkDuplicate};
+}
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
